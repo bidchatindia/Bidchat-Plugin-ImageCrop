72a11f797affb5e6f7b90646094979be8ef678b0
Tushar Vengurlekar <tushar.vengurlekar@bidchat.com>
2016-07-17 20:48:17 +0530

    Fixed Crash due to File Path and removed Warnings

--- src/android/CropPlugin.java
+++ src/android/CropPlugin.java
@@ -8,6 +8,8 @@ import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.util.Log;
 
+import com.yalantis.ucrop.UCrop;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -15,49 +17,49 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.yalantis.ucrop.UCrop;
-
 import java.io.File;
 
 public class CropPlugin extends CordovaPlugin {
 
-    public final static int PICK_IMAGE = 25001, CROP_IMAGE = 955212;
     private CallbackContext callbackContext;
     private Uri inputUri;
     private Uri outputUri;
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-      if (action.equals("cropImage")) {
-          int width = getDeviceWidth(), height = 0;
-          int quality = 100;
-          if (args.length() > 1)
-          {
-              quality = args.getJSONObject(1).getInt("quality");
-              height = args.getJSONObject(1).getInt("imageHeight");
-          }
-          String imagePath = args.getString(0);
+        if (action.equals("cropImage")) {
+            int width = getDeviceWidth(), height = getDeviceWidth();
+            int quality = 0;
+            if (args.length() > 1) {
+                quality = args.getJSONObject(1).getInt("quality");
+                height = args.getJSONObject(1).getInt("imageHeight");
+            }
+            String imagePath = args.getString(0);
 
-          this.inputUri = Uri.parse(imagePath);
-          this.outputUri = Uri.fromFile(new File(getTempDirectoryPath() + "/" + System.currentTimeMillis()+ "-cropped.jpg"));
+            height = height == 0 ? width : height;
+            quality = quality == 0 ? 100 : quality;
 
-          PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
-          pr.setKeepCallback(true);
-          callbackContext.sendPluginResult(pr);
-          this.callbackContext = callbackContext;
-          Log.i("Reached here", "Before start");
-          cordova.setActivityResultCallback(this);
-          UCrop.Options options = new UCrop.Options();
-          options.setHideBottomControls(true);
-          options.withMaxResultSize(width, height);
-          UCrop.of(this.inputUri , this.outputUri)
-                  .withOptions(options)
-                  .withAspectRatio(width, height)
-                  .withMaxResultSize(width, height)
-                  .start(cordova.getActivity());
-          return true;
-      }
-      return false;
+            this.inputUri = Uri.parse("file://" + imagePath);
+            this.outputUri = Uri.fromFile(new File(getTempDirectoryPath() + "/" + System.currentTimeMillis() + "-cropped.jpg"));
+
+            PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
+            pr.setKeepCallback(true);
+            callbackContext.sendPluginResult(pr);
+            this.callbackContext = callbackContext;
+            Log.i("Reached here", "Before start");
+            cordova.setActivityResultCallback(this);
+            UCrop.Options options = new UCrop.Options();
+            options.setHideBottomControls(true);
+            options.withMaxResultSize(width, height);
+            options.setCompressionQuality(quality);
+            UCrop.of(this.inputUri, this.outputUri)
+                    .withOptions(options)
+                    .withAspectRatio(width, height)
+                    .withMaxResultSize(width, height)
+                    .start(cordova.getActivity());
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -65,10 +67,13 @@ public class CropPlugin extends CordovaPlugin {
         if (requestCode == UCrop.REQUEST_CROP) {
             if (resultCode == Activity.RESULT_OK) {
                 Uri imageUri = UCrop.getOutput(intent);
-                this.callbackContext.success("file://" + imageUri.getPath());
+                if (imageUri != null) {
+                    this.callbackContext.success("file://" + imageUri.getPath());
+                } else {
+                    this.callbackContext.error("Image not found");
+                }
                 this.callbackContext = null;
-            } 
-            else if (resultCode == UCrop.RESULT_ERROR) {
+            } else if (resultCode == UCrop.RESULT_ERROR) {
                 try {
                     JSONObject err = new JSONObject();
                     err.put("message", "Error on cropping");
@@ -78,8 +83,7 @@ public class CropPlugin extends CordovaPlugin {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-            } 
-            else if (resultCode == Activity.RESULT_CANCELED) {
+            } else if (resultCode == Activity.RESULT_CANCELED) {
                 try {
                     JSONObject err = new JSONObject();
                     err.put("message", "User cancelled");
@@ -94,15 +98,14 @@ public class CropPlugin extends CordovaPlugin {
         super.onActivityResult(requestCode, resultCode, intent);
     }
 
-    int getDeviceWidth()
-    {
+    int getDeviceWidth() {
         DisplayMetrics metrics = new DisplayMetrics();
         cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
         return metrics.widthPixels;
     }
 
     private String getTempDirectoryPath() {
-        File cache = null;
+        File cache;
 
         // SD Card Mounted
         if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
@@ -115,8 +118,10 @@ public class CropPlugin extends CordovaPlugin {
         }
 
         // Create the cache directory if it doesn't exist
+        //noinspection ResultOfMethodCallIgnored
         cache.mkdirs();
         return cache.getAbsolutePath();
+
     }
 
     public Bundle onSaveInstanceState() {
